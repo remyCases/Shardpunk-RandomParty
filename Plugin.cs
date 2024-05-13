5edb5d15e1b2f4eec1ebe9bf586daeaf2ea155bd
remyCases <remy.cases@gmail.com>
2024-05-13 19:29:04 +0200

    [Major] it's alive, and working

---
+++ Plugin.cs
@@ -0,0 +1,155 @@
+﻿// Copyright (C) 2024 Rémy Cases
+// See LICENSE file for extended copyright information.
+// This file is part of the Speedshard repository from https://github.com/remyCases/Shardpunk-RandomParty.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Assets.Scripts;
+using Assets.Scripts.GameUI;
+using Assets.Scripts.GameUI.MainMenu;
+using Assets.Scripts.Localisation;
+using Assets.Scripts.Logic.Tactical;
+using BepInEx;
+using HarmonyLib;
+using HarmonyLib.Tools;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Random;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+public class Plugin : BaseUnityPlugin
+{
+    private void Awake()
+    {
+        // Plugin startup logic
+        HarmonyFileLog.Enabled = true;
+        Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        Harmony harmony = new(PluginInfo.PLUGIN_GUID);
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch]
+public static class RandomPatch
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(PartyCreationModalDisplayBehaviour), "Awake")]
+    static bool PartyCreationModalDisplayBehaviourAwake(PartyCreationModalDisplayBehaviour __instance)
+    {
+        // find the element to copy
+        // maybe a better button should be used, but it works
+        GameObject buttonPrefab = GameObject.Find("GoreRoot");
+        // new button root !
+        GameObject randomButton = UnityEngine.Object.Instantiate(buttonPrefab, __instance.transform.GetChild(1).GetChild(0));
+        randomButton.name = "RandomButtonRoot";
+        randomButton.transform.SetSiblingIndex(1);
+
+        // remove the gore component
+        PartyCreationGoreToggleDisplayBehavior goreScript = randomButton.GetComponent<PartyCreationGoreToggleDisplayBehavior>();
+        UnityEngine.Object.Destroy(goreScript);
+        // remove the header text
+        UnityEngine.Object.DestroyImmediate(randomButton.transform.GetChild(1).gameObject);
+
+        // add the new component
+        RandomDisplayBehavior randomDisplay = randomButton.AddComponent<RandomDisplayBehavior>();
+        randomDisplay.enabled = true;
+
+        // change the name of the button
+        Transform button = randomButton.transform.GetChild(0);
+        button.name = "RandomButton";
+
+        return true;
+    }
+}
+
+public class RandomDisplayBehavior : GameEventBehaviourBase
+{
+    public void Awake()
+    {
+        this.ToggleButton = this.transform.GetComponentInChildren<GameButtonDisplayBehaviour>();
+        this.ToggleButton.OnClick.AddListener(new UnityAction(this.ApplyRandom));
+        this.UpdateDisplay();
+    }
+
+    protected override void OnGameEvent(object sender, EventArgs e)
+    {
+        if (e is PartySelectionUIStateChangedArgs)
+        {
+            this.UpdateDisplay();
+        }
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+        this.UpdateDisplay();
+    }
+
+    private void ApplyRandom()
+    {
+        // all available elements
+        Game instance = Game.Instance;
+        PartySelectionUIState partySelectionState = instance.MainMenuUIState.PartySelectionState;
+        PartySelectionCharacterItem[] chars = AvailableCharactersProvider.Instance.GetAvailableCharacters();
+        PartySelectionCharacterItem[] bots = AvailableCharactersProvider.Instance.GetAvailableBotCharacters();
+
+        // clear party
+        partySelectionState.Party.ClearAll();
+
+        // add randoms to party
+        partySelectionState.PartyBotSlot = RandomUtils.SelectionSamplingTechnique(bots, 1).First().Character;
+        foreach(Character c in RandomUtils.SelectionSamplingTechnique(chars, partySelectionState.MaxCharactersInParty).Select(x => x.Character))
+        {
+            PartySelection.AddOrRemoveCharacterToParty(c);
+        }
+        this.UpdateDisplay();
+    }
+
+    private void UpdateDisplay()
+    {
+        this.ToggleButton.Mode = ButtonColorMode.MinorBlue;
+        this.ToggleButton.Text.text = GameLoc.Instance.Get("gameplaySettings_randomize");
+    }
+
+    public GameButtonDisplayBehaviour ToggleButton;
+}
+
+public static class RandomUtils
+{
+    // from Knuth Donald, The Art Of Computer Programming, Volume 2, Third Edition
+    // 3.4.2 Random Sampling and Shuffling, p142
+    // Algorithm S
+    public static IEnumerable<T> SelectionSamplingTechnique<T>(this IList<T> list, int n)
+    {
+        System.Random rand = new();
+        // number of elements dealt with
+        int tt = 0;
+        // number of elements selected by the algorithm
+        int m = 0;
+        int N = list.Count;
+        // firewall if we want more elements than the size of the list
+        int nn = Math.Min(n, N);
+
+        double u;
+        
+        while(m < nn)
+        {
+            u = rand.NextDouble();
+
+            if((N - tt)*u >= nn - m) 
+            {
+                // element not selected
+                tt++;
+            }
+            else
+            {
+                // element selected
+                yield return list[tt];
+                tt++;
+                m++;
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,58 @@
+# Shardpunk - RandomParty
+
+## Description
+
+This mod adds a new button in the party selection menu to select a random party among all unlocked units.
+
+## Installation
+
+This mod was made with `BepInEx` which is why you need to install it first.
+
+### Installing BepInEx
+
+- Download the version `5.4.22` of [BepInEx](https://github.com/BepInEx/BepInEx/releases/tag/v5.4.22).
+- Unzip it in the `Shardpunk` folder.
+
+### Installing BiggerTeam
+
+- Download the latest release of [RandomParty](https://github.com/remyCases/Shardpunk-RandomParty/releases). 
+- Unzip it, you should have 2 files: `Random.dll` and `base.txt`.
+- Move `Random.dll` in the `Shardpunk/BepInEx/plugins` folder.
+- Move `base.txt` in the `Data\Languages\en` folder.
+
+You can now play the game with the modded version !
+
+## Troubleshooting
+
+If you encountered some troubles while playing with this mod, you can contact me on [Discord](https://discord.com/users/200330865522376704), and send me the log file `LogOutput` found in the folder `Shardpunk/BepInEx`.
+
+## For anyone who wants to change this mod and build it themself
+
+> [!CAUTION]
+> The current repo can not be built directly as I didn't include the Assembly dll of Shardpunk (for obvious reasons).
+> You need to create a `lib` folder first and to move the `Assembly-CSharp.dll` and the `UnityEngine.UI.dll` from the `Shardpunk/Shardpunk_Data/Managed` folder to the `lib` folder.
+
+This mod was made using `HarmonyX`, a fork of `Harmony2` from `BepInEx`. I strongly advise anyone who wants to change the mod to check the documentation of [Harnomy2](https://harmony.pardeike.net/articles/intro.html) and to check the difference between [Harmony2 and HarmonyX](https://github.com/BepInEx/HarmonyX/wiki/Difference-between-Harmony-and-HarmonyX) first.
+
+## See also
+
+Other mods I've made:
+- Shardpunk:
+    - [Shardpunk-Faster](https://github.com/remyCases/Shardpunk-Faster)
+    - [Shardpunk-MoreSkillLevels](https://github.com/remyCases/Shardpunk-MoreSkillLevels)
+    - [Shardpunk-Faster](https://github.com/remyCases/Shardpunk-Faster)
+
+- Stoneshard:
+    - [Character Creation](https://github.com/remyCases/CharacterCreator)
+    - [Defeat Scenarios](https://github.com/remyCases/Stoneshard-DefeatScenarios)
+    - [More Save Slots](https://github.com/remyCases/Stoneshard-MoreSaveSlots)
+    - [Pelt Durability](https://github.com/remyCases/Stoneshard-PeltDurability)
+    - [Speedshard_Core](https://github.com/remyCases/SpeedshardCore)
+    - [Speedshard_Sprint](https://github.com/remyCases/SpeedshardSprint)
+    - [Speedshard_Backpack](https://github.com/remyCases/SpeedshardBackpack)
+    - [Speedshard_Skinning](https://github.com/remyCases/SpeedshardSkinning)
+    - [Speedshard_MoneyDungeon](https://github.com/remyCases/SpeedshardMoneyDungeon)
+    - [Speedshard_Stances](https://github.com/remyCases/SpeedshardStances)
+
+- Airship Kingdom Adrift:
+    - [ProductionPanel](https://github.com/remyCases/AKAMod_ProdPanel)
